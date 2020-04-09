debc2a1ebc2404ec775b2573e5a6d52649d4b7bf
Philip Chang <sgnoohc@gmail.com>
2020-04-08 18:02:40 -0700

    update

--- src/Process_4LepMET.cc
+++ src/Process_4LepMET.cc
@@ -15,6 +15,12 @@ void Process_4LepMET()
     ana.tx.setBranch<float>("4LepMET_intVar1", -999);
     ana.tx.setBranch<float>("4LepMET_floatVar1", -999);
 
+    // Example of reading from Nano
+    // std::vector<LorentzVector> electron_p4s = nt.Electron_p4(); // nt is a global variable that accesses NanoAOD
+    // std::vector<float> electron_mvaTTH = nt.Electron_mvaTTH(); // electron ttH MVA scores from NanoAOD
+    // Semi-complete list of NanoAOD for 102X can be found here: https://cms-nanoaod-integration.web.cern.ch/integration/master-102X/mc102X_doc.html
+    // Also consult here: https://github.com/cmstas/NanoTools/blob/d641a6d6c1aa9ecc8094a1af73d5e1bd7d6502ab/NanoCORE/Nano.h#L4875 (if new variables are added they may show up in master)
+
     LorentzVector LV_4LepMET_LVVar1 = RooUtil::Calc::getLV(34.5, 1.2, 3.123, 0.105); // RooUtil::Calc::getLV() creates 4 vector
 
     ana.tx.setBranch<LorentzVector>("4LepMET_LVVar1", LV_4LepMET_LVVar1);
